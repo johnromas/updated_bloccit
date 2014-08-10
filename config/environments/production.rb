69b86252245d01d744c583257fdcf48ef9b5b0ec
vagrant <john.romas87@gmail.com>
2014-08-10 23:28:19 +0000

    Fixed production mail host bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Jbloccit::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'romas-bloccit.herokuapp.com' }
 end
